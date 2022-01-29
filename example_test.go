02ad3e944e37f1c3703fdd299d15a71b462ce2b5
Guilherme Siani <contact@guilhermesiani.com>
2022-01-29 19:29:16 -0300

    feat: add test asserts

--- example_test.go
+++ example_test.go
@@ -3,11 +3,13 @@ package codeowners_test
 import (
 	"bytes"
 	"fmt"
+    "testing"
 
 	"github.com/hmarr/codeowners"
+    "github.com/stretchr/testify/assert"
 )
 
-func Example() {
+func TestExample(t *testing.T) {
 	f := bytes.NewBufferString("src/**/*.c @acme/c-developers")
 	ruleset, err := codeowners.ParseFile(f)
 	if err != nil {
@@ -24,41 +26,31 @@ func Example() {
 	// <nil>
 }
 
-func ExampleParseFile() {
+func TestExampleParseFile(t *testing.T) {
 	f := bytes.NewBufferString("src/**/*.go @acme/go-developers # Go code")
 	ruleset, err := codeowners.ParseFile(f)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(len(ruleset))
-	fmt.Println(ruleset[0].RawPattern())
-	fmt.Println(ruleset[0].Owners[0].String())
-	fmt.Println(ruleset[0].Comment)
-	// Output:
-	// 1
-	// src/**/*.go
-	// @acme/go-developers
-	// Go code
+    assert.Len(t, ruleset, 1)
+    assert.Equal(t, "src/**/*.go", ruleset[0].RawPattern())
+    assert.Equal(t, "@acme/go-developers", ruleset[0].Owners[0].String())
+    assert.Equal(t, "Go code", ruleset[0].Comment)
 }
 
-func ExampleRuleset_Match() {
+func TestExampleRuleset_Match(t *testing.T) {
 	f := bytes.NewBufferString("src/**/*.go @acme/go-developers # Go code")
 	ruleset, _ := codeowners.ParseFile(f)
 
 	match, _ := ruleset.Match("src")
-	fmt.Println("src", match != nil)
+    assert.Nil(t, match)
 
 	match, _ = ruleset.Match("src/foo.go")
-	fmt.Println("src/foo.go", match != nil)
+    assert.NotNil(t, match)
 
 	match, _ = ruleset.Match("src/foo/bar.go")
-	fmt.Println("src/foo/bar.go", match != nil)
+    assert.NotNil(t, match)
 
 	match, _ = ruleset.Match("src/foo.rs")
-	fmt.Println("src/foo.rs", match != nil)
-	// Output:
-	// src false
-	// src/foo.go true
-	// src/foo/bar.go true
-	// src/foo.rs false
+    assert.Nil(t, match)
 }
